0ee60f52f4951d2901125199d98d4bc54ce99122
Song SJun <songsjun@SongdeMacBook-Pro.local>
2019-04-22 00:23:22 +0800

    change domain name

--- src/util.js
+++ src/util.js
@@ -13,7 +13,7 @@ const config = {
   appDidPublicKey: '02752F9483DF73C57EDEA1F84F2431DC1036B2643F9519E78CB660D8C332793EDC',
   appDidPrivateKey: '462B93F275E0458F838BFAC195EE32BBD21D71DE794938572A9F9F5FF7AE80D6',
   appDidMnemonic: 'quote milk ring ketchup refuse chief float please water march car tone',
-  callbackUrl : 'https://liyangwood.github.io/dpos_vote/#/return_url',
+  callbackUrl : 'https://elaphantapp.github.io/dposvote/#/return_url',
   // callbackUrl : 'http://192.168.1.17:8080/#/return_url',
   // callbackUrl : '',
   random : '998877'
