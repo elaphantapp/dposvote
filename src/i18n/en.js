b2f301e9cf5c3a6ee87ef25aefbe053b153864a7
songjiazhun <869077613@qq.com>
2019-04-26 22:13:07 +0800

    fix ui 16547582

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -2,54 +2,56 @@ import _ from 'lodash';
 import C from './cty';
 
 const country = {};
-_.each(C, (item)=>{
-  _.set(country, item.code, item.en);
+_.each(C, (item) => {
+    _.set(country, item.code, item.en);
 });
 
 export default {
-  country,
+    country,
 
-  VOTING : 'Voting',
-  RANK : 'Rank',
-  FAV : 'Favorites',
-  LATEST : 'Latest',
-  NODES : 'Nodes',
-  FAVORITES : 'Favorites',
-  MY_VOTES : 'My Votes',
-  NODE_DETAILS : 'Node Details',
-  SEARCH : 'search',
-  VOTE : 'Vote',
-  VOTING_POWER_USED : 'voting power Used',
-  TOTAL : 'Total',
-  RULE : 'Rules',
-  ALL : 'All',
-  SUCCESS : 'Success',
-  FAILURE : 'Failure',
-  VOTING_RULE : 'Voting Rules',
-  rule_msg : {
-    '1' : '1 ELA may be used to vote for a maximum of 36 different nodes and 1 ELA may only give each node a maximum of 1 vote;',
-    '2' : 'When a user spends ELA, the votes corresponding to the spent ELA are revoked. However, an option is available that allows the user to vote again with its new balance after spending ELA.',
-    link : 'https://news.elastos.org/elastos-dpos-supernode-election-process/'
-  },
-  MORE_DETAIL : 'Further Details Available at',
-  RE_VOTE : 'Re-Vote',
-  VOTES : 'Votes',
-  STATUS : 'Status',
-  LOCATION : 'Location',
-  YEAR : 'year',
-  EST : 'EST',
-  PUBLIC_KEY : 'Public Key',
-  CANCEL : 'Cancel',
-  CLOSE : 'Close',
+    VOTING: 'Voting',
+    RANK: 'Rank',
+    FAV: 'Favorites',
+    LATEST: 'Latest',
+    NODES: 'Nodes',
+    FAVORITES: 'Favorites',
+    MY_VOTES: 'My Votes',
+    NODE_DETAILS: 'Node Details',
+    SEARCH: 'search',
+    VOTE: 'Vote',
+    VOTING_POWER_USED: 'voting power Used',
+    TOTAL: 'Total',
+    RULE: 'Rules',
+    ALL: 'All',
+    SUCCESS: 'Success',
+    FAILURE: 'Failure',
+    VOTING_RULE: 'Voting Rules',
+    rule_msg: {
+        '1': '1 ELA may be used to vote for a maximum of 36 different nodes and 1 ELA may only give each node a maximum of 1 vote;',
+        '2': 'When a user spends ELA, the votes corresponding to the spent ELA are revoked. However, an option is available that allows the user to vote again with its new balance after spending ELA.',
+        link: 'https://news.elastos.org/elastos-dpos-supernode-election-process/'
+    },
+    MORE_DETAIL: 'Further Details Available at',
+    RE_VOTE: 'Re-Vote',
+    VOTES: 'Votes',
+    STATUS: 'Status',
+    LOCATION: 'Location',
+    YEAR: 'year',
+    EST: 'EST',
+    PUBLIC_KEY: 'Public Key',
+    CANCEL: 'Cancel',
+    CLOSE: 'Close',
 
-  '01' : 'votes',
-  '02' : 'Vote',
-  '03' : 'Votes %',
-  '04' : 'Votes gap',
-  '05' : 'copy success',
+    '01': 'votes',
+    '02': 'Vote',
+    '03': 'Votes %',
+    '04': 'Votes gap',
+    '05': 'copy success',
+    '06': 'Active Node Cutoff',
+    '07': 'Standby Node Cutoff',
 
 
-  Activate: 'Active',
-  Inactivate : 'Inactive',
-  Pengding : 'Pending'
+    Activate: 'Active',
+    Inactivate: 'Inactive',
+    Pengding: 'Pending'
 };
--- src/i18n/zh.js
+++ src/i18n/zh.js
@@ -2,52 +2,54 @@ import _ from 'lodash';
 import C from './cty';
 
 const country = {};
-_.each(C, (item)=>{
-  _.set(country, item.code, item.zh);
+_.each(C, (item) => {
+    _.set(country, item.code, item.zh);
 });
 export default {
-  country,
+    country,
 
-  VOTING : '投票',
-  RANK : '排名',
-  FAV : '最爱',
-  LATEST : '最新',
-  NODES : '节点',
-  FAVORITES : '最爱',
-  MY_VOTES : '我的投票',
-  NODE_DETAILS : '节点信息',
-  SEARCH : '搜索',
-  VOTE : '票',
-  VOTING_POWER_USED : '投票权 已用',
-  TOTAL : '总共',
-  RULE : '规则',
-  ALL : '全部',
-  SUCCESS : '成功',
-  FAILURE : '失败',
-  VOTING_RULE : '投票规则',
-  rule_msg : {
-    '1' : '1个ELA最多给36个不同的节点投票，1个ELA给同一个节点最多只能投1票；',
-    '2' : '如果投票之后进行转出交易，会导致之前的投票失效，需要再次进行投票；您也可以在转出交易的同时勾选自动投票。',
-    link : 'https://news-zh.elastos.org/亦来云dpos超级节点竞选细则/'
-  },
-  MORE_DETAIL : '更多信息',
-  RE_VOTE : '重新投票',
-  VOTES : '投票',
-  STATUS : '状态',
-  LOCATION : '位置',
-  YEAR : '年',
-  EST : '预估',
-  PUBLIC_KEY : '节点公钥',
-  CANCEL : '取消',
-  CLOSE : '关闭',
+    VOTING: '投票',
+    RANK: '排名',
+    FAV: '最爱',
+    LATEST: '最新',
+    NODES: '节点',
+    FAVORITES: '最爱',
+    MY_VOTES: '我的投票',
+    NODE_DETAILS: '节点信息',
+    SEARCH: '搜索',
+    VOTE: '票',
+    VOTING_POWER_USED: '投票权 已用',
+    TOTAL: '总共',
+    RULE: '规则',
+    ALL: '全部',
+    SUCCESS: '成功',
+    FAILURE: '失败',
+    VOTING_RULE: '投票规则',
+    rule_msg: {
+        '1': '1个ELA最多给36个不同的节点投票，1个ELA给同一个节点最多只能投1票；',
+        '2': '如果投票之后进行转出交易，会导致之前的投票失效，需要再次进行投票；您也可以在转出交易的同时勾选自动投票。',
+        link: 'https://news-zh.elastos.org/亦来云dpos超级节点竞选细则/'
+    },
+    MORE_DETAIL: '更多信息',
+    RE_VOTE: '重新投票',
+    VOTES: '投票',
+    STATUS: '状态',
+    LOCATION: '位置',
+    YEAR: '年',
+    EST: '预估',
+    PUBLIC_KEY: '节点公钥',
+    CANCEL: '取消',
+    CLOSE: '关闭',
 
-  '01' : '票',
-  '02' : '投票',
-  '03' : '票数占比',
-  '04' : '投票差距',
-  '05' : '拷贝成功',
+    '01': '票',
+    '02': '投票',
+    '03': '票数占比',
+    '04': '投票差距',
+    '05': '拷贝成功',
+    '06': '',
+    '07': '',
 
-  Activate: '可投票',
-  Inactivate : '可投票（非活跃）',
-  Pengding : '不可投票（注册中）'
+    Activate: '可投票',
+    Inactivate: '可投票（非活跃）',
+    Pengding: '不可投票（注册中）'
 };
