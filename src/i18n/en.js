749d38e90ed910dd3ef9dbd8bb5dbd04b8bcbda5
song sjun <song.sjun@hotmail.com>
2019-04-26 18:13:48 +0800

    update english words

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -21,14 +21,14 @@ export default {
   VOTE : 'Vote',
   VOTING_POWER_USED : 'voting power Used',
   TOTAL : 'Total',
-  RULE : 'Rule',
+  RULE : 'Rules',
   ALL : 'All',
   SUCCESS : 'Success',
   FAILURE : 'Failure',
   VOTING_RULE : 'Voting Rules',
   rule_msg : {
     '1' : '1 ELA may be used to vote for a maximum of 36 different nodes and 1 ELA may only give each node a maximum of 1 vote;',
-    '2' : 'When a user spends ELA, the votes corresponding to the spent ELA are revoked, However, an option is available that allows the user to vote again with its new balance after spending ELA.',
+    '2' : 'When a user spends ELA, the votes corresponding to the spent ELA are revoked. However, an option is available that allows the user to vote again with its new balance after spending ELA.',
     link : 'https://news.elastos.org/elastos-dpos-supernode-election-process/'
   },
   MORE_DETAIL : 'Further Details Available at',
@@ -46,7 +46,7 @@ export default {
   '02' : 'Vote',
   '03' : 'Votes %',
   '04' : 'Votes gap',
-  '05' : 'copy successs',
+  '05' : 'copy success',
 
 
   Activate: 'Active',
