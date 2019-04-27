70ef532be2af93967dce7f8ea1d4d5ad76eec632
songjiazhun <869077613@qq.com>
2019-04-27 17:26:17 +0800

    add nodelist fitter compatible code 00

--- src/store.js
+++ src/store.js
@@ -2,248 +2,248 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import util from '@/util';
 import fake from '@/fake';
-import {vue} from './main';
+import { vue } from './main';
 
 import request from './request';
 
 Vue.use(Vuex);
 
 const F = {
-  processNodeList(list, total){
-    const fav_list = F.getFavList();
-    return util._.map(list, (item)=>{
-      if(util._.isUndefined(item.selected)){
-        item.selected = false;
-      }
-      item.Location = item.Location.toString();
-      item.id = item.Producer_public_key || item.Address || item.Nodepublickey;
-      item.Percentage = 0; 
-      if(total){
-        item.Percentage = Math.fround(((item.Value || 0)/total)*100).toFixed(2);
-      }
-      item.EstRewardPerYear = parseFloat(item.EstRewardPerYear).toFixed(2).toString();
-      item.Value = parseFloat(item.Value || 0).toFixed(2);
+    processNodeList(list, total) {
+        const fav_list = F.getFavList();
+        return util._.map(list, (item) => {
+            if (util._.isUndefined(item.selected)) {
+                item.selected = false;
+            }
+            item.Location = item.Location.toString();
+            item.id = item.Producer_public_key || item.Address || item.Nodepublickey;
+            item.Percentage = 0;
+            if (total) {
+                item.Percentage = Math.fround(((item.Value || 0) / total) * 100).toFixed(2);
+            }
+            item.EstRewardPerYear = parseFloat(item.EstRewardPerYear).toFixed(2).toString();
+            item.Value = parseFloat(item.Value || 0).toFixed(2);
 
-      const flag = util._.findIndex(fav_list, (l)=>{
-        return l.Producer_public_key === item.Producer_public_key;
-      });
-      item.fav = flag !== -1;
-      if(item.fav){
-        item.selected = true;
-      }
+            const flag = util._.findIndex(fav_list, (l) => {
+                return l.Producer_public_key === item.Producer_public_key;
+            });
+            item.fav = flag !== -1;
+            if (item.fav) {
+                item.selected = true;
+            }
 
-      return item;
-    });
-  },
-  getFavList(){
-    const list = util.ls.get('fav_list');
-    if(!list) return [];
+            return item;
+        });
+    },
+    getFavList() {
+        const list = util.ls.get('fav_list');
+        if (!list) return [];
 
-    return list;
-  },
-  setFavList(list){
-    util.ls.set('fav_list', list);
-  }
+        return list;
+    },
+    setFavList(list) {
+        util.ls.set('fav_list', list);
+    }
 };
 
 export default new Vuex.Store({
-  state: {
-    node_list : null,
-    current_node : null,
-    node_detail : null,
-    my_votes_list : null,
-    my_vote_detail : null,
-    my_vote_detail_list : null,
-    me_info : null,
-    my_fav_list : null,
+    state: {
+        node_list: null,
+        current_node: null,
+        node_detail: null,
+        my_votes_list: null,
+        my_vote_detail: null,
+        my_vote_detail_list: null,
+        me_info: null,
+        my_fav_list: null,
 
-    current_tab : 'tab1',
+        current_tab: 'tab1',
 
-    node_page_filter : 1,   // 1 rank, 2 fav, 3 latest, 4 asc
-    node_page_search : '',
+        node_page_filter: 1, // 1 rank, 2 fav, 3 latest, 4 asc
+        node_page_search: '',
 
-    global : {
-      total_vote : null
-    }
-  },
-  mutations: {
-    set_node_list(state, list){
-      let tmp = F.processNodeList(list, state.global.total_vote);
-      if(state.node_page_search){
-        tmp = util._.filter(tmp, (item)=>{
-          return _.includes(item.Nickname.toLowerCase(), state.node_page_search.toLowerCase());
-        });
-      }
-      if(state.node_page_filter === 2){
-        state.node_list = util._.filter(tmp, (item)=>{
-          return item.fav;
-        })
-      }
-      else if(state.node_page_filter === 4){
-        state.node_list = tmp.sort((a, b)=>{
-          return a.Nickname.charCodeAt(0) - b.Nickname.charCodeAt(0)
-        });
-      }
-      else{
-        state.node_list = tmp;
-      }
-
-      console.log('node_list', state.node_page_filter, state.node_list)
+        global: {
+            total_vote: null
+        }
     },
-    set_current_node(state, node){
-      state.current_node = node;
-    },
-    set_node_detail(state, node_detail){
-      state.node_detail = node_detail;
-    },
-    set_my_votes_list(state, list){
-      state.my_votes_list = list;
-    },
-    set_me_info(state, info){
-      state.me_info = info;
-    },
-    set_my_vote_detail(state){
-      const map = {};
-      const list = state.my_votes_list;
-      util._.each(list, (item)=>{
-        item.list = F.processNodeList(item.Vote_Body, state.global.total_vote);
-        item.list = util._.map(item.list, (item)=>{
-          item.selected = true;
-          return item;
-        });
-        map[item.Vote_Header.Txid] = item;
-
-      });
-      state.my_vote_detail = map;
-      
-    },
-    set_my_fav_list(state, list){
-      state.my_fav_list = list;
-    },
-
-    set_tab(state, tab){
-      state.current_tab = tab;
-    },
-
-    set_node_page_filter(state, filter){
-      state.node_page_filter = filter;
-    },
-
-    set_node_page_search(state, search){
-      state.node_page_search = search;
-    },
-
-    set_global(state, param){
-      const tmp = util._.extend(state.global, param||{});
-      Vue.set(state.global, tmp);
-    }
-  },
-  actions: {
-    set_node_list({ commit, state }, param){
-      const cache_list = util.rc.get('node_list');
-      if(cache_list){
-        commit('set_node_list', cache_list);
-        return false;
-      }
-
-      request.getCurrentBlockHeight().then((d)=>{
-        const height = d.result;
-        request.getTotalVotes(height).then((d)=>{
-          console.log(2, d);
-          commit('set_global', {
-            total_vote : d.result
-          });
-          request.getNodeList(height).then((d)=>{
-            util.rc.set('node_list', util._.clone(d.result));
-            console.log(1, d)
-            const list = d.result;
-            commit('set_node_list', list);
-          })
-        })
-        
-      })
-
-    },
-
-
-    set_node_detail({commit, state}, id){
-      commit('set_node_detail', null);
-      const json = util._.find(state.node_list, (item)=>{
-        return item.id === id;
-      });
-      commit('set_node_detail', json);
-
-      // request.getNodeInfoByAddress(id).then((d)=>{
-      //   console.log(2, d);
-      // })
-    },
-
-    set_me_info({commit}){
-      commit('set_me_info', {});
-      const data = util.getUserData();
-      data.vp_used = 0;
-      // data.Data.ELAAddress = 'ENaaqePNBtrZsNbs9uc35CPqTbvn8oaYL9'
-      request.getElaByAddress(data.Data.ELAAddress).then((d)=>{
-        
-        data.ela_total = d.result;
-        
-        request.getVoteByAddress(data.Data.ELAAddress).then((d)=>{
-          util._.each(d.result, (item)=>{
-            // console.log(parseFloat(item.Vote_Header.Value))
-            if(item.Vote_Header.Is_valid && item.Vote_Header.Is_valid === 'YES'){
-              data.vp_used += parseFloat(item.Vote_Header.Value);
-              
+    mutations: {
+        set_node_list(state, list) {
+            let tmp = F.processNodeList(list, state.global.total_vote);
+            if (state.node_page_search) {
+                tmp = util._.filter(tmp, (item) => {
+                    return _.includes(item.Nickname.toLowerCase(), state.node_page_search.toLowerCase());
+                });
             }
-            
-          })
+            if (state.node_page_filter === 2) {
+                state.node_list = util._.filter(tmp, (item) => {
+                    return item.fav;
+                })
+            } else if (state.node_page_filter === 4) {
+                state.node_list = tmp.sort((a, b) => {
+                    return a.Nickname.charCodeAt(0) - b.Nickname.charCodeAt(0)
+                });
+            } else {
+                state.node_list = tmp;
+            }
 
-          console.log('my_votes_list', d.result);
-          commit('set_my_votes_list', d.result);
-          commit('set_my_vote_detail');
-        })
+            console.log('node_list', state.node_page_filter, state.node_list)
+        },
+        set_current_node(state, node) {
+            state.current_node = node;
+        },
+        set_node_detail(state, node_detail) {
+            state.node_detail = node_detail;
+        },
+        set_my_votes_list(state, list) {
+            state.my_votes_list = list;
+        },
+        set_me_info(state, info) {
+            state.me_info = info;
+        },
+        set_my_vote_detail(state) {
+            const map = {};
+            const list = state.my_votes_list;
+            util._.each(list, (item) => {
+                item.list = F.processNodeList(item.Vote_Body, state.global.total_vote);
+                item.list = util._.map(item.list, (item) => {
+                    item.selected = true;
+                    return item;
+                });
+                map[item.Vote_Header.Txid] = item;
 
-        console.log('me_info', data);
-        commit('set_me_info', data);
-      });
+            });
+            state.my_vote_detail = map;
+
+        },
+        set_my_fav_list(state, list) {
+            state.my_fav_list = list;
+        },
+
+        set_tab(state, tab) {
+            state.current_tab = tab;
+        },
+
+        set_node_page_filter(state, filter) {
+            state.node_page_filter = filter;
+        },
+
+        set_node_page_search(state, search) {
+            state.node_page_search = search;
+        },
+
+        set_global(state, param) {
+            const tmp = util._.extend(state.global, param || {});
+            Vue.set(state.global, tmp);
+        }
     },
+    actions: {
+        set_node_list({ commit, state }, param) {
+            const cache_list = util.rc.get('node_list');
+            if (cache_list) {
+                commit('set_node_list', cache_list);
+                return false;
+            }
+
+            request.getCurrentBlockHeight().then((d) => {
+                const height = d.result;
+                request.getTotalVotes(height).then((d) => {
+                    console.log(2, d);
+                    commit('set_global', {
+                        total_vote: d.result
+                    });
+                    request.getNodeList(height).then((d) => {
+                        var nodelist = util.handleNodeList(d.result);
+                        console.log(JSON.stringify(nodelist));
+                        util.rc.set('node_list', util._.clone(nodelist));
+                        console.log(1, d)
+                        const list = nodelist;
+                        commit('set_node_list', list);
+                    })
+                })
+
+            })
+
+        },
 
 
-    set_my_fav_list({commit}, param){
-      const list = F.getFavList();
-      commit('set_my_fav_list', list);
-    },
+        set_node_detail({ commit, state }, id) {
+            commit('set_node_detail', null);
+            const json = util._.find(state.node_list, (item) => {
+                return item.id === id;
+            });
+            commit('set_node_detail', json);
 
-    addFavItem(store, item){
-      let list = F.getFavList();
+            // request.getNodeInfoByAddress(id).then((d)=>{
+            //   console.log(2, d);
+            // })
+        },
 
-      const x = util._.find(list, (x)=>{
-        return x.Producer_public_key === item.Producer_public_key;
-      })
-      if(x){
-        util.toastInfo('success');
-        return false;
-      }
+        set_me_info({ commit }) {
+            commit('set_me_info', {});
+            const data = util.getUserData();
+            data.vp_used = 0;
+            // data.Data.ELAAddress = 'ENaaqePNBtrZsNbs9uc35CPqTbvn8oaYL9'
+            request.getElaByAddress(data.Data.ELAAddress).then((d) => {
 
-      list.unshift(item);
+                data.ela_total = d.result;
 
-      list = list.sort((a, b)=>{
-        return a.Index - b.Index;
-      })
+                request.getVoteByAddress(data.Data.ELAAddress).then((d) => {
+                    util._.each(d.result, (item) => {
+                        // console.log(parseFloat(item.Vote_Header.Value))
+                        if (item.Vote_Header.Is_valid && item.Vote_Header.Is_valid === 'YES') {
+                            data.vp_used += parseFloat(item.Vote_Header.Value);
 
-      F.setFavList(list);
-      util.toastInfo(vue.$t('SUCCESS'));
-      
-      
-    },
-    removeFavItem(store, param){
-      const list = F.getFavList();
-      util._.remove(list, (item)=>{
-        return item.id === param.id;
-      });
-      
-      F.setFavList(list);
-      util.toastInfo(vue.$t('SUCCESS'));
-      
+                        }
+
+                    })
+
+                    console.log('my_votes_list', d.result);
+                    commit('set_my_votes_list', d.result);
+                    commit('set_my_vote_detail');
+                })
+
+                console.log('me_info', data);
+                commit('set_me_info', data);
+            });
+        },
+
+
+        set_my_fav_list({ commit }, param) {
+            const list = F.getFavList();
+            commit('set_my_fav_list', list);
+        },
+
+        addFavItem(store, item) {
+            let list = F.getFavList();
+
+            const x = util._.find(list, (x) => {
+                return x.Producer_public_key === item.Producer_public_key;
+            })
+            if (x) {
+                util.toastInfo('success');
+                return false;
+            }
+
+            list.unshift(item);
+
+            list = list.sort((a, b) => {
+                return a.Index - b.Index;
+            })
+
+            F.setFavList(list);
+            util.toastInfo(vue.$t('SUCCESS'));
+
+
+        },
+        removeFavItem(store, param) {
+            const list = F.getFavList();
+            util._.remove(list, (item) => {
+                return item.id === param.id;
+            });
+
+            F.setFavList(list);
+            util.toastInfo(vue.$t('SUCCESS'));
+
+        }
     }
-  }
 })
--- src/util.js
+++ src/util.js
@@ -6,184 +6,202 @@ import { Toast } from 'mint-ui';
 
 
 const config = {
-  appId : '23091883A390CCBFFFED4928F996936AFCEBB1B57192532D15271158F3A277FD1BB3309DA2719334CBE1DE7BA2408047E2786A94F370CE66C208159B3A8D1162',
-  appName : 'dopsvote.h5.app',
-  appDid : 'iZW9ozTSXk4ukRXx7vCTTFYebZHFwMUtz7',
-  appDidPublicKey: '03128A35842DD061BD016B9B2913BE07028063E5A80365D713DB77508406E85815',
-  callbackUrl : 'https://elaphantapp.github.io/dposvote/#/return_url',
-  random : '998877'
+    appId: '23091883A390CCBFFFED4928F996936AFCEBB1B57192532D15271158F3A277FD1BB3309DA2719334CBE1DE7BA2408047E2786A94F370CE66C208159B3A8D1162',
+    appName: 'dopsvote.h5.app',
+    appDid: 'iZW9ozTSXk4ukRXx7vCTTFYebZHFwMUtz7',
+    appDidPublicKey: '03128A35842DD061BD016B9B2913BE07028063E5A80365D713DB77508406E85815',
+    callbackUrl: 'https://elaphantapp.github.io/dposvote/#/return_url',
+    random: '998877'
 };
 const _user_data = {
-  ...config
+    ...config
 };
 
 
 
-const isChrome = ()=>{
-  const ua = navigator.userAgent.toLowerCase()
-  return ua.indexOf('chrome') !== -1 && ua.indexOf('android') === -1;
+const isChrome = () => {
+    const ua = navigator.userAgent.toLowerCase()
+    return ua.indexOf('chrome') !== -1 && ua.indexOf('android') === -1;
 }
 
-const getUrlParam = (name, url)=>{
-  url = url || location.hash;
-  const t = url.replace('#/return_url', '').replace(/%09/g, '');
-  const reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-  const r = decodeURIComponent(t).substr(1).match(reg);
-  if (r) return unescape(r[2]);
-  return null;
+const getUrlParam = (name, url) => {
+    url = url || location.hash;
+    const t = url.replace('#/return_url', '').replace(/%09/g, '');
+    const reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    const r = decodeURIComponent(t).substr(1).match(reg);
+    if (r) return unescape(r[2]);
+    return null;
 }
 
 const _cache = {};
 const rc = {
-  set(key, rs){
-    const data = {
-      data : rs,
-      t : Date.now()
+    set(key, rs) {
+        const data = {
+            data: rs,
+            t: Date.now()
+        }
+        _.set(_cache, key, data);
+    },
+    get(key) {
+        const rs = _.get(_cache, key, null);
+        if (!rs) return null;
+        const data = rs.data;
+        if (Date.now() - rs.t > 1000 * 60 * 30) {
+            return null;
+        }
+
+        return data;
     }
-    _.set(_cache, key, data);
-  },
-  get(key){
-    const rs = _.get(_cache, key, null);
-    if(!rs) return null;
-    const data = rs.data;
-    if(Date.now() - rs.t > 1000*60*30){
-      return null;
-    }
-    
-    return data;
-  }
 };
 
 const ls = {
-  set(key, value){
-    const d = {
-      data : value,
-      t : Date.now()
-    };
-    localStorage.setItem(key, JSON.stringify(d));
-  },
-  get(key){
-    const rs = localStorage.getItem(key);
-    if(!rs) return null;
-    const json = JSON.parse(rs);
-    return json.data;
-  }
+    set(key, value) {
+        const d = {
+            data: value,
+            t: Date.now()
+        };
+        localStorage.setItem(key, JSON.stringify(d));
+    },
+    get(key) {
+        const rs = localStorage.getItem(key);
+        if (!rs) return null;
+        const json = JSON.parse(rs);
+        return json.data;
+    }
 };
 window._cache = _cache;
 window._user_data = _user_data;
-export default{
-  _,
-  moment,
-  rc,
-  ls,
-  isChrome,
-  loading(flag=false){
-    if(flag){
-      Indicator.open({
-        text: 'Loading...',
-        spinnerType: 'fading-circle'
-      });
-      _.delay(()=>{
-        Indicator.close();
-      }, 2000);
+export default {
+    _,
+    moment,
+    rc,
+    ls,
+    isChrome,
+    loading(flag = false) {
+        if (flag) {
+            Indicator.open({
+                text: 'Loading...',
+                spinnerType: 'fading-circle'
+            });
+            _.delay(() => {
+                Indicator.close();
+            }, 2000);
+        } else {
+            Indicator.close();
+        }
+    },
+
+    request(param = {}, mockResult = {}) {
+        return new Promise((resolve) => {
+            _.delay(() => {
+                resolve(mockResult);
+            }, 1000)
+        })
+
+    },
+
+    register: PubSub.subscribe,
+    publish: PubSub.publish,
+
+    toastSuccess(msg) {
+        Toast({
+            message: msg,
+            className: 'kg-toast',
+            iconClass: 'fa fa-check'
+        });
+    },
+
+    toastInfo(msg) {
+        Toast({
+            message: msg,
+            className: 'kg-toast',
+            position: 'bottom',
+            duration: 3000
+        });
+    },
+
+    getConfig() {
+        return config;
+    },
+    getUserData() {
+        // const d = ls.get('user-data');
+        const d = _user_data;
+        if (!d || !d['Sign']) {
+            return null;
+        }
+
+        return d;
+    },
+    setUserData(queryString) {
+        queryString = queryString || location.hash;
+
+        const Data = getUrlParam('Data', queryString);
+
+        const tmp = JSON.parse(decodeURIComponent(Data));
+        tmp.Sign = getUrlParam('Sign', queryString);
+
+        const d = {
+            Data: tmp,
+            PublicKey: tmp.PublicKey,
+            Sign: tmp.Sign
+        };
+        _.extend(_user_data, d);
+        console.log(2, _user_data);
+        // ls.set('user-data', _user_data);
+    },
+
+    getUrlParam,
+
+
+    buildRequestUserDataUrl() {
+        const d = _user_data;
+
+        const rt = encodeURIComponent(d.callbackUrl);
+        let url = `elaphant://identity?ReturnUrl=${rt}&AppID=${d.appId}&PublicKey=${d.appDidPublicKey}&DID=${d.appDid}&RandomNumber=${d.random}&AppName=${d.appName}&RequestInfo=elaaddress`;
+
+        console.log('login schema => ' + url);
+        location.href = url;
+
+
+        return url;
+    },
+
+    buildVoteSchema(nodePublicKeyList) {
+        const d = _user_data;
+
+        const to = _.map(nodePublicKeyList, (k) => {
+            return '"' + k + '"';
+        }).join(',');
+
+        const rt = encodeURIComponent(d.callbackUrl);
+        const pp = (`[${to}]`);
+        let url = `elaphant://eladposvote?AppID=${d.appId}&PublicKey=${d.appDidPublicKey}&Signature=${d.appSign}&DID=${d.appDid}&AppName=${d.appName}&ReturnUrl=${rt}&CandidatePublicKeys=${pp}`;
+
+
+
+        console.log('vote schema => ' + url);
+        location.href = url;
+
+        return url;
+    },
+
+    handleNodeList(nodeList) {
+        var arr = [];
+        for (var index = 0; index < nodeList.length; index++) {
+            var item = nodeList[index];
+            if (item["State"] === "pending" || item["State"] === "Activate" || item["State"] === "inactive") {
+                var location = item["Location"].toString();
+                if (location.substring(0, 2) === "00") {
+                    item["Location"] = parseInt(location.substring(2));
+                }
+                arr.push(item);
+            }
+        }
+
+        return arr;
     }
-    else{
-      Indicator.close();
-    }
-  },
-
-  request(param={}, mockResult={}){
-    return new Promise((resolve)=>{
-      _.delay(()=>{
-        resolve(mockResult);
-      }, 1000)
-    })
-
-  },
-
-  register: PubSub.subscribe,
-  publish: PubSub.publish,
-
-  toastSuccess(msg){
-    Toast({
-      message: msg,
-      className: 'kg-toast',
-      iconClass: 'fa fa-check'
-    });
-  },
-
-  toastInfo(msg){
-    Toast({
-      message: msg,
-      className: 'kg-toast',
-      position: 'bottom',
-      duration: 3000
-    });
-  },
-
-  getConfig(){
-    return config;
-  },
-  getUserData(){
-    // const d = ls.get('user-data');
-    const d = _user_data;
-    if(!d || !d['Sign']){
-      return null;
-    }
-
-    return d;
-  },
-  setUserData(queryString){
-    queryString = queryString || location.hash;
-
-    const Data = getUrlParam('Data', queryString);
- 
-    const tmp = JSON.parse(decodeURIComponent(Data));
-    tmp.Sign = getUrlParam('Sign', queryString);
-    
-    const d = {
-      Data : tmp,
-      PublicKey : tmp.PublicKey,
-      Sign : tmp.Sign
-    };
-    _.extend(_user_data, d);
-    console.log(2, _user_data); 
-    // ls.set('user-data', _user_data);
-  },
-
-  getUrlParam,
 
 
-  buildRequestUserDataUrl(){
-    const d = _user_data;
 
-    const rt = encodeURIComponent(d.callbackUrl);
-    let url = `elaphant://identity?ReturnUrl=${rt}&AppID=${d.appId}&PublicKey=${d.appDidPublicKey}&DID=${d.appDid}&RandomNumber=${d.random}&AppName=${d.appName}&RequestInfo=elaaddress`;
-
-    console.log('login schema => '+url);
-    location.href = url;
-    
-    
-    return url;
-  },
-
-  buildVoteSchema(nodePublicKeyList){
-    const d = _user_data;
-
-    const to = _.map(nodePublicKeyList, (k)=>{
-      return '"'+k+'"';
-    }).join(',');
-
-    const rt = encodeURIComponent(d.callbackUrl);
-    const pp = (`[${to}]`);
-    let url = `elaphant://eladposvote?AppID=${d.appId}&PublicKey=${d.appDidPublicKey}&Signature=${d.appSign}&DID=${d.appDid}&AppName=${d.appName}&ReturnUrl=${rt}&CandidatePublicKeys=${pp}`;
-
-    
-
-    console.log('vote schema => '+url);
-    location.href = url;
-    
-    return url;
-  }
 
 }
